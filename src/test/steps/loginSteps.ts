37522c87d426cb69e3c9ca4ca02a06d1b2a67ef9
logunlaja26 <logunlaja26@gmail.com>
2025-11-26 18:34:18 -0600

    Create page fixture hook to make code reusable

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,43 +1,43 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { Page,Browser,chromium } from "@playwright/test";
+import { Browser,chromium } from "@playwright/test";
+import { pageFixture } from "../../hooks/pageFixture";
 
 let browser: Browser;
-let page: Page;
 Given('User navigates to the application', async function () {
     browser = await chromium.launch({ headless: true });
-    page = await browser.newPage();
-    await page.goto('https://www.bstackdemo.com/', { 
+    pageFixture.page = await browser.newPage();
+    await pageFixture.page.goto('https://www.bstackdemo.com/', { 
       waitUntil: 'domcontentloaded', // or 'load' or 'networkidle'
       timeout: 60000 
     });
   });
 
 Given('User click on the login link', async function () {
-  await page.locator('span').filter({ hasText: 'Sign In' }).click();
+  await pageFixture.page.locator('span').filter({ hasText: 'Sign In' }).click();
 });
 
 When('User enter the username as {string}', async function (username) {
-  await page.getByText('Select Username').press('Tab');
-  await page.keyboard.type(username);
+  await pageFixture.page.getByText('Select Username').press('Tab');
+  await pageFixture.page.keyboard.type(username);
 });
 
 
 Given('User enter the password as {string}', async function (password) {
-  await page.getByText('Select Password').press('Tab');
-  await page.keyboard.type(password);
+  await pageFixture.page.getByText('Select Password').press('Tab');
+  await pageFixture.page.keyboard.type(password);
 });
 
 When('User click on the login button', async function () {
-  await page.locator("//button[@id='login-btn']").click();
+  await pageFixture.page.locator("//button[@id='login-btn']").click();
 });
 
 Then('Login should be success', async function () {
-  await page.locator('span').filter({ hasText: 'Logout' }).isVisible();
+  await pageFixture.page.locator('span').filter({ hasText: 'Logout' }).isVisible();
   await browser.close();
 });
 
 Then('Login should fail', async function () {
-  await page.locator('h3').filter({ hasText: 'Invalid Username' }).isVisible();
+  await pageFixture.page.locator('h3').filter({ hasText: 'Invalid Username' }).isVisible();
   await browser.close();
 });
   
