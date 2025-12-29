25da0f4c8c8dbbda2ea5f5feb512d9c66c0391b2
logunlaja26 <logunlaja26@gmail.com>
2025-12-29 14:59:31 -0600

    code refactoring

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,35 +1,36 @@
 import { BeforeAll, AfterAll, Before, After, Status } from "@cucumber/cucumber";
-import { Browser,BrowserContext,chromium } from "@playwright/test";
+import { Browser, BrowserContext, chromium } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
 let context: BrowserContext;
 
 BeforeAll(async function () {
-    browser = await chromium.launch({ 
-        headless: !false,
-        slowMo: 1000 // Slows down actions by 1000ms (1 second) between each action
-    });
+  browser = await chromium.launch({
+    headless: true,
+    slowMo: 1000, // Slows down actions by 1000ms (1 second) between each action
+  });
 });
 
 Before(async function () {
-    context = await browser.newContext({
-        permissions: ['geolocation']
-    });
-    const page = await context.newPage();
-    pageFixture.page = page;
+  context = await browser.newContext({
+    permissions: ["geolocation"],
+  });
+  const page = await context.newPage();
+  pageFixture.page = page;
 });
 
 After(async function (this: any, { pickle, result }) {
-    if (result?.status === Status.FAILED) {
-        const img = await pageFixture.page.screenshot({ path: `test-results/screenshot-${pickle.name}-${Date.now()}.png` });
-        await this.attach(img, "image/png");
-    }
-    await pageFixture.page.close();
-    await context.close();
-})
-
+  if (result?.status === Status.FAILED) {
+    const img = await pageFixture.page.screenshot({
+      path: `test-results/screenshot-${pickle.name}-${Date.now()}.png`,
+    });
+    await this.attach(img, "image/png");
+  }
+  await pageFixture.page.close();
+  await context.close();
+});
 
 AfterAll(async function () {
-    await browser.close();
-})
+  await browser.close();
+});
--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,36 +1,29 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { pageFixture } from "../../hooks/pageFixture";
 import { expect } from "@playwright/test";
+import { LoginPage } from "../../pages/loginPage";
 
-Given('User navigates to the application', async function () {
-    pageFixture.page.goto('https://www.bstackdemo.com/', { 
-      waitUntil: 'domcontentloaded',
-      timeout: 60000 
-    });
-  });
+let loginPage: LoginPage;
 
-Given('User click on the login link', async function () {
-  await pageFixture.page.locator('span').filter({ hasText: 'Sign In' }).click();
+Given("User navigates to the application", async function () {
+  loginPage = new LoginPage(pageFixture.page);
+  await loginPage.navigateToLoginPage();
 });
 
-When('User enter the username as {string}', async function (username) {
-  await pageFixture.page.getByText('Select Username').press('Tab');
-  await pageFixture.page.keyboard.type(username);
+Given("User click on the login link", async function () {
+  await pageFixture.page.locator("span").filter({ hasText: "Sign In" }).click();
 });
 
+When("User enter the username as {string}", async function (username) {
+  await loginPage.enterUsername(username);
+});
 
 Given('User enter the password as {string}', async function (password) {
-  await pageFixture.page.getByText('Select Password').press('Tab');
-  await pageFixture.page.locator('div.css-tlfecz-indicatorContainer:visible').click();
-  await pageFixture.page.getByText(password, { exact: true }).click();
+  await loginPage.enterPassword(password);
 });
 
-
-Given('User enter incorrect password as {string}', async function (password) {  
-  await pageFixture.page.getByText('Select Password').press('Tab');
-  await pageFixture.page.locator('div.css-tlfecz-indicatorContainer:visible').click();
-  await pageFixture.page.keyboard.type(password);
-  await pageFixture.page.keyboard.press('Enter');
+Given('User enter incorrect password as {string}', async function (password) {
+  await loginPage.enterIncorrectPassword(password);
 });
 
 When('User click on the login button', async function () {
